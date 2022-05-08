a7c570df11be9cb9be3582052f1b4bfceaf81142
Jarek Radosz <jradosz@gmail.com>
2022-05-08 12:37:12 +0200

    Drop es6 suffix

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
 # name: twitter-topic-group-button
 # about: adds a custom button at the bottom of a topic, visible only to staff or members of a specific group
 # version: 0.1
 # authors: Robin Ward, Arpit Jalan
+# transpile_js: true
 
 after_initialize do
   add_to_serializer(:current_user, :can_see_topic_group_button?) do
