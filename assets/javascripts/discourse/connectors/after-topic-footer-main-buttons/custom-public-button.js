99af19670694c843fe39ef864c2e9e892a188c0c
Jarek Radosz <jradosz@gmail.com>
2022-05-08 12:38:47 +0200

    linting

--- assets/javascripts/discourse/connectors/after-topic-footer-main-buttons/custom-public-button.js
+++ assets/javascripts/discourse/connectors/after-topic-footer-main-buttons/custom-public-button.js
@@ -1,16 +1,19 @@
 export default {
   shouldRender(args, component) {
     const { currentUser } = component;
-    return currentUser && currentUser.can_see_topic_group_button;
+    return currentUser?.can_see_topic_group_button;
   },
 
   actions: {
     clickButton() {
       const topic = this.args.topic;
       const user = this.currentUser;
+      const url = this.siteSettings.topic_group_button_url
+        .replace("<TOPIC_ID>", topic.get("model.id"))
+        .replace("<USER_ID>", user.id)
+        .replace("<USERNAME>", user.username);
 
-      const url = this.siteSettings.topic_group_button_url.replace('<TOPIC_ID>', topic.get('model.id')).replace('<USER_ID>', user.id).replace('<USERNAME>', user.username);
-      window.open(url, '_blank');
-    }
-  }
-}
+      window.open(url, "_blank");
+    },
+  },
+};
